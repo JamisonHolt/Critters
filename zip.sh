24252ab10fce2979d9d094f671e4c8b0ae8578ae
JamisonHolt <JamisonHolt95@gmail.com>
2018-03-22 22:09:49 -0500

    Added Zipping Feature

---
+++ zip.sh
@@ -0,0 +1,5 @@
+cp ./src/assignment4/Critter.java ./Project4_JAH7327/src/assignment4/
+cp ./src/assignment4/Critter1.java ./Project4_JAH7327/src/assignment4/
+cp ./src/assignment4/Critter2.java ./Project4_JAH7327/src/assignment4/
+cp ./src/assignment4/Main.java ./Project4_JAH7327/src/assignment4/
+zip -r Project4_JAH7327.zip ./Project4_JAH7327
